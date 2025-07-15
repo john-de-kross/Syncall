1b2744154d0057be7f0270ebfe989908a515fb56
john-de-kross <chukwujekwujohncross@gmail.com>
2025-07-15 12:49:31 +0100

    changes

--- src/LANDINGPAGE/index.jsx
+++ src/LANDINGPAGE/index.jsx
@@ -1,17 +1,32 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Copy, Link, Phone } from "lucide-react";
+import { Tooltip } from 'react-tooltip'
 
 const LandingPage = () => {
+    const [clickCopy, setClickCopy] = useState(false);
+
+    const handleCopyTouch = () => {
+        setClickCopy(true);   
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (clickCopy) {
+                setClickCopy(false)
+            }
+        }, 1000)
+
+    }, [clickCopy])
     return (
-        <div className="w-full min-h-screen bg-gradient-to-b from-blue-400 to-blue-900  flex items-center justify-center md:bg-slate-700 md:bg-none md:from-none md:to-none">
+        <div className="w-full min-h-screen md:mx-auto md:w-[90%] md:[70vh] bg-gradient-to-b from-blue-400 to-blue-900  flex items-center justify-center md:bg-slate-700 md:bg-none md:from-none md:to-none">
             <div className="w-full h-full md:w-[60%] md:h-[75vh] md:bg-gradient-to-br md:from-slate-600 md:via-slate-700 md:to-indigo-500 md:grid md:py-7 md:grid-cols-2 md:rounded-lg md:shadow-lg">
                 <div className="h-[40vh] brand-text md:w-[90%] md:h-[65vh] text-white text-sm shadow-lg shadow-black rounded-r-[20%] bg-gradient-to-b from-blue-400 to-blue-900 flex flex-col justify-center items-center text-center gap-2">
-                    <h1 className="text-2xl mb-4 font-bold py-2">Welcome to Syncall</h1>
+                    <h1 className="text-2xl mb-4 font-bold py-2 fade-in">Welcome to Syncall</h1>
                     <p className="text-lg font-light text-gray-100">The ultimate video call app</p>
                     <p className="text-lg font-light text-gray-100">Enjoy your seamless communication with family & friends.</p>
                     <div className="hidden md:flex md:items-center md:justify-center md:h-60 md:mt-1 md:w-full ">
-                        <img className="h-full" src="/video-call.png" alt="video-call" />
+                        <img className="h-full phone" src="/video-call.png" alt="video-call" />
                     </div>
                 </div>
                 <div className="w-full mt-3 md:w-[80%] md:h-[60vh] flex flex-col place-items-center md:items-start">
@@ -24,7 +39,7 @@ const LandingPage = () => {
                             Username
                         </label>
                         <input
-                            className="outline-none border text-white p-2 w-full h-9 md:h-8 bg-gradient-to-r from-gray-700 to-gray-900 border-gray-500 rounded-lg"
+                            className="outline-none border text-white p-2 w-full h-12 md:h-10 bg-gradient-to-r from-gray-700 to-gray-900 border-gray-500 rounded-lg"
                             type="text"
                             placeholder="'eg: Michael' "
                         />
@@ -32,11 +47,20 @@ const LandingPage = () => {
                             Unique ID
                         </label>
                         <input
-                            className="outline-none relative border text-white p-2 w-full h-9 md:h-8 bg-gradient-to-r from-gray-700 to-gray-900 border-gray-500 rounded-lg"
+                            className="outline-none relative border text-white p-2 w-full h-12 md:h-10 bg-gradient-to-r from-gray-700 to-gray-900 border-gray-500 rounded-lg"
                             type="text"
                             placeholder="abc12345"
                         />
-                        < Copy className="absolute mt-28 md:mt-30 w-4 h-4 right-[12%] md:cursor-pointer text-gray-200 md:right-[30%]" />
+                        < Copy
+                            onClick={handleCopyTouch}
+                            className="absolute mt-32 md:mt-33 w-4 h-4 outline-none right-[12%] md:cursor-pointer text-gray-200 md:right-[30%]"
+                            data-tooltip-id="copy-tooltip"
+                            data-tooltip-content='Copied!'
+                            data-tooltip-override="true" 
+                        />
+                        <Tooltip id="copy-tooltip" className="tooltip" open={clickCopy} />
+                    
+                        
                         <p className="text-xs w-full flex justify-center text-gray-400 px-4">Tip: Unique ID is automatically copied</p>
                         <div className="btn w-full flex flex-col md:flex-row place-items-center gap-4 md:flex md:justify-between md:gap-4">
                             <button className="flex justify-center gap-3 items-center font-normal md:font-medium text-gray-100 shadow rounded-xl shadow-slate-900 bg-blue-950 w-[60%] md:w-[70%] h-9 md:rounded-2xl">
