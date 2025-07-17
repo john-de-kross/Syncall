ccd7445732751a8ee6201ced0088b602f22027f9
john-de-kross <chukwujekwujohncross@gmail.com>
2025-07-17 13:58:16 +0100

    added video cam

--- src/ROOM/index.jsx
+++ src/ROOM/index.jsx
@@ -15,9 +15,25 @@ import {
   Video,
   Volume2,
 } from "lucide-react";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const Room = () => {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const contraints = { video: true, audio: true };
+
+        navigator.mediaDevices.getUserMedia(contraints)
+            .then((stream) => {
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+            }
+            
+            }).catch((err) => {
+            console.log("Media err", err)
+        })
+        
+    }, [])
   return (
     <div className="w-full flex flex-col min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-800">
       <nav className="flex items-center justify-between px-4 md:px-4 h-20 md:h-40 bg-gradient-to-r from-slate-700 lg:hidden via-black to-gray-900">
@@ -34,9 +50,12 @@ const Room = () => {
           </span>
           <ChevronDown size={20} className="text-gray-300 md:h-14 md:w-14" />
         </div>
-        <div className="bg-red-500 h-5 w-5 rounded md:h-8 md:w-8"></div>
+              <div className="bg-red-500 h-5 w-5 rounded md:h-8 md:w-8">          
+        </div>
       </nav>
-      <main className="flex flex-grow video-containe w-full bg-red-500"></main>
+        <main className="flex flex-grow video-contain w-full">
+            <video ref={videoRef} autoPlay playsInline className="w-full h-full" />
+      </main>
       <footer className=" ">
         <div className="flex overflow-auto justify-evenly w-full gap-8 items-center px-4 h-20 bg-gradient-to-r from-gray-800 via-black to-gray-950">
           <div className="flex flex-col gap-1 items-center">
